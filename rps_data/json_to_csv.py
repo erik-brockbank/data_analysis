3a1fe1aa5050c8c12a60a1f4b506bf3a0a868a08
Erik Brockbank <erikbrockbank@eriks-mbp.attlocal.net>
2019-08-26 13:42:10 -0700

    Add processing of individual files for use with RPS_Data tooling

--- rps_data/json_to_csv.py
+++ rps_data/json_to_csv.py
@@ -7,29 +7,42 @@ from os.path import isfile, join
 EXPERIMENT = "rps" # useful identifier for experiment data: modify this to reflect the particular experiment
 DATA_PATH = "/Users/erikbrockbank/web/vullab/rps/data/" # path to data files: modify as needed for particular experiments
 OUTPUT_FILE = "{}_raw.csv".format(EXPERIMENT) # name of csv file to write to
+INDIVID_PATH = "individual_files/" # pathway writing individual game files
 
+with io.open(OUTPUT_FILE, "w") as csv_output:
+    csvwriter = csv.writer(csv_output)
+    write_index = 0
+    files = [f for f in listdir(DATA_PATH) if f.endswith(".json") and not "TEST" in f]
+    for f in files:
+        with io.open(join(DATA_PATH + f), "r", encoding = "utf-8", errors = "ignore") as readfile:
+            print("Processing: {}".format(f))
+            content = readfile.read()
+            parsed_data = json.loads(content)
+            round_data = parsed_data["rounds"]
 
-csv_output = open(OUTPUT_FILE, "w")
-csvwriter = csv.writer(csv_output)
+            if write_index == 0:
+                header = [] # init header array
+                header.extend(round_data[0].keys())
+                csvwriter.writerow(header)
+                write_index = 1
 
-write_index = 0
-files = [f for f in listdir(DATA_PATH) if f.endswith(".json")]
-for f in files:
-    with io.open(join(DATA_PATH + f), "r", encoding = "utf-8", errors = "ignore") as readfile:
-        print("Processing: {}".format(f))
-        content = readfile.read()
-        parsed_data = json.loads(content)
-        round_data = parsed_data["rounds"]
+            for r in round_data:
+                vals = [] # init data array
+                vals.extend(r.values())
+                csvwriter.writerow(vals)
 
-        if write_index == 0:
+        # write results of this game to individual game csv (for use with RPS_Data visualizing)
+        output_individ = f.split(".")[0] + ".csv"
+        with io.open(join(INDIVID_PATH + output_individ), "w") as csv_output_individ:
+            csvwriter_individ = csv.writer(csv_output_individ)
             header = [] # init header array
             header.extend(round_data[0].keys())
-            csvwriter.writerow(header)
-            write_index = 1
+            csvwriter_individ.writerow(header)
+            for r in round_data:
+                vals = [] # init data array
+                vals.extend(r.values())
+                csvwriter_individ.writerow(vals)
+        csv_output_individ.close()
 
-        for r in round_data:
-            vals = [] # init data array
-            vals.extend(r.values())
-            csvwriter.writerow(vals)
 
 csv_output.close()
