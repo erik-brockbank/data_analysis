87c229cd94e5d2fb0e16c60f03754d7be67ceb13
Erik Brockbank <erikbrockbank@eriks-mbp.attlocal.net>
2019-08-30 16:48:49 -0600

    early analysis of rps data

--- rps_data/data_analysis.R
+++ rps_data/data_analysis.R
@@ -10,10 +10,67 @@ rm(list = ls())
 setwd("/Users/erikbrockbank/web/vullab/data_analysis/rps_data/")
 
 source('data_processing.R') # script used for data processing/cleanup
+glimpse(data)
 
 
-#' TODO entropy analyses for each game_id
+##########################
+### ANALYSIS FUNCTIONS ###
+##########################
 
-#' TODO how do we know people are trying to *win*? Subjective random choice seems like defense, not offense
 
+# Function to summarize probability of each move for each participant
+get.move.probs = function(data) {
+  data %>%
+    group_by(player_id) %>%
+    count(player_move) %>%
+    mutate(total = sum(n),
+           pmove = n / total)
+}
+
+# Function to summarize probability of each move for each participant, 
+# conditioned on their previous move
+get.prior.move.probs = function(data) {
+  data %>%
+    group_by(player_id) %>%
+    mutate(prev.move = lag(player_move, 1)) %>%
+    filter(!is.na(prev.move)) %>% # lag call above sets NA for lag on very first move: ignore it here
+    group_by(player_id, player_move, prev.move) %>%
+    mutate(move_prev.move = paste0(player_move, "-", prev.move)) %>% # category of move given previous move, e.g. "rock-paper"
+    group_by(player_id) %>%
+    count(move_prev.move) %>%
+    mutate(total.moves = sum(n),
+           pmove_prev.move = n / total.moves) %>% # probability of this category of move, e.g. probability of "rock-paper"
+    group_by(player_id, move_prev.move) %>%
+    mutate(player_move = strsplit(move_prev.move, "-")[[1]][1], # add prev.move back in because we lose it in the count() call above
+      prev.move = strsplit(move_prev.move, "-")[[1]][2]) # add prev.move back in because we lose it in the count() call above
+}
+
+
+################
+### ANALYSIS ###
+################
+
+
+### First order analysis: p(move = x) ###
+player.summary = get.move.probs(data)
+
+player.entropy = player.summary %>%
+  group_by(player_id) %>%
+  summarize(overall.entropy = -sum(pmove * log10(pmove))) # get entropy for distribution of each move
+
+
+
+### Second order analysis: p(move = x | prev. move = y) ###
+player.prev.move.summary = get.prior.move.probs(data)
+
+player.prev.move.entropy = player.prev.move.summary %>%
+  group_by(player_id, prev.move) %>%
+  summarize(prev.move.entropy = -sum(pmove_prev.move * log10(pmove_prev.move))) %>% # get entropy for distribution of each move, for each prev. move
+  inner_join(player.summary, by = "player_id") %>%
+  mutate(prev.move.entropy.norm = prev.move.entropy * pmove) %>% # normalize entropy of each move given each prev. move by the probability of that prev. move
+  group_by(player_id, prev.move) %>%
+  filter(prev.move == player_move) %>%
+  group_by(player_id) %>%
+  summarize(overall.entropy.pmove = sum(prev.move.entropy.norm)) # sum normalized entropy for distribution of each move given each prev. move
+  
 
--- rps_data/data_processing.R
+++ rps_data/data_processing.R
@@ -25,12 +25,15 @@ DATA_FILE = "rps_raw.csv" # name of file containing full dataset for all rounds
 read.data = function(filename) {
   # read aggregate data
   data = read_csv(filename)
-  
-  # process data
-  data$player_move = as.factor(data$player_move)
-  data$player_outcome = as.factor(data$player_outcome)
-  
   return(data)
+}
+
+# Function to calculate Shannon entropy for proportions of R, P, S
+# requires that data come in with a "player_move" col and a "move_pct" col
+# returns entropy value for that set of move proportions
+get.entropy = function(proportions) {
+  # S = - SUM_i { p_i * log(p_i) } for probability vector p of R, P, S
+  -sum(proportions$move_pct * log10(proportions$move_pct))
 }
 
 
@@ -61,17 +64,17 @@ mylogy = function(lims){
 
 individ_plot_theme = theme(
   # titles
-  plot.title = element_text(face = "bold", size = 28),
-  axis.title.y = element_text(face = "bold", size = 24),
-  axis.title.x = element_text(face = "bold", size = 24),
+  plot.title = element_text(face = "bold", size = 24),
+  axis.title.y = element_text(face = "bold", size = 20),
+  axis.title.x = element_text(face = "bold", size = 20),
   legend.title = element_text(face = "bold", size = 16),
   # axis text
-  axis.text.y = element_text(size = 14),
-  axis.text.x = element_text(size = 14, angle = 90, hjust = 0, vjust = 0),
+  axis.text.y = element_text(size = 12),
+  axis.text.x = element_text(size = 12, angle = 90, hjust = 0, vjust = 0),
   # legend text
   legend.text = element_text(size = 14),
   # facet text
-  strip.text = element_text(face = "bold", size = 20),
+  strip.text = element_text(size = 12),
   # backgrounds, lines
   panel.background = element_blank(),
   strip.background = element_blank(),
@@ -83,9 +86,9 @@ individ_plot_theme = theme(
 )
 
 
-####################
-### DATA SUMMARY ###
-####################
+##########################
+### GRAPHING FUNCTIONS ###
+##########################
 
 # Check that each player's response times aren't so low that they may not be trying
 plot.rt = function(data) {
@@ -99,6 +102,25 @@ plot.rt = function(data) {
     theme(axis.text.x = element_blank())
 }
 
+# Check that each player's response times stay relatively consistent over the experiment
+plot.rt.sequence = function(data) {
+  quartile.data = data %>%
+    group_by(player_id) %>%
+    mutate(round_quartile = ntile(round_index, 4)) %>%
+    ungroup() %>%
+    group_by(round_quartile) %>%
+    summarize(quartile.mean = mean(player_rt),
+              quartile.se = sd(player_rt) / sqrt(length(player_rt)))
+  
+  quartile.data %>%
+    ggplot(aes(x = round_quartile, y = quartile.mean)) +
+    geom_point() +
+    geom_errorbar(aes(ymin = quartile.mean - quartile.se, ymax = quartile.mean + quartile.se)) +
+    labs(x = "Trial quartile", y = "Participant response times") +
+    ggtitle("Change in participant response times by quartile") +
+    individ_plot_theme
+}
+
 
 # Check that each player is doing a reasonable distribution of moves
 plot.moves = function(data) {
@@ -106,7 +128,8 @@ plot.moves = function(data) {
     group_by(player_id) %>%
     count(player_move) %>%
     mutate(total = sum(n),
-           move_pct = n / total)
+           move_pct = n / total,
+           participant_abbrev = strsplit(player_id, "-")[[1]][5])
   
   move.summary %>%
     ggplot(aes(x = player_move, y = move_pct)) +
@@ -115,8 +138,41 @@ plot.moves = function(data) {
     labs(x = "Move", y = "Percent") +
     ggtitle("Move percentage by participant") +
     individ_plot_theme +
-    theme(strip.text = element_blank()) +
-    facet_wrap(~player_id, scales = "free_y", ncol = 3)
+    facet_wrap(~participant_abbrev, scales = "free_y", ncol = 4)
+}
+
+# Check that entropy of each player's moves stays relatively consistent
+# -> If it goes down substantially, this is a sign they may have stopped trying
+plot.move.sequence = function(data) {
+  subsets = 10
+  subset.data = data %>%
+    group_by(player_id) %>%
+    mutate(round_ntile = ntile(round_index, subsets)) %>%
+    group_by(player_id, round_ntile) %>%
+    count(player_move) %>%
+    mutate(total = sum(n),
+           move_pct = n / total,
+           participant_abbrev = strsplit(player_id, "-")[[1]][5])
+  
+  # there has to be a way to do this without looping...
+  entropy.seq = data.frame(participant_abbrev = character(), subset = numeric(), entropy = double())
+  for (player in unique(subset.data$participant_abbrev)) {
+    for (subset in seq(1:subsets)) {
+      subj.ntile.data = subset.data %>% 
+        filter(participant_abbrev == player, round_ntile == subset)
+      entropy.seq = rbind(entropy.seq, data.frame(participant_abbrev = player, subset = subset,
+                                                  entropy = get.entropy(subj.ntile.data)))
+    }
+  }
+
+  entropy.seq %>%
+    ggplot(aes(x = subset, y = entropy)) +
+    geom_line() +
+    labs(x = "ntile of trials", y = "Shannon entropy (S)") +
+    ggtitle("Entropy of participant moves over trial ntile") +
+    individ_plot_theme +
+    facet_wrap(~participant_abbrev, scales = "free_y", ncol = 4)
+
 }
 
 # Check that no player did so terribly that we should be suspicious of whether they were trying
@@ -153,14 +209,23 @@ table(data$player_id)
 
 
 ### Response times ###
-# Plot RT distributions
-plot.rt(data)
-# Manually view the lowest RTs across participants
+plot.rt(data) # 100 trial participants look a lot like 300 trial participants
 sort(data$player_rt)[1:50] # we have some very low RTs...
+plot.rt.sequence(data) # by quartile, people do seem to be going faster: are they caring less?
 
 
 ### Move choices ###
-plot.moves(data)
+plot.moves(data) # some of these people don't appear to have a stable balance of R, P, S: did they stop trying?
+
+chisq_tests = data.frame(player_id = character(), chisq = numeric(), p_val = numeric())
+for (player in unique(data$player_id)) {
+  subj.data = data %>%
+    filter(player_id == player,
+           player_move %in% c("rock", "paper", "scissors"))
+  tst = chisq.test(table(subj.data$player_move))
+  chisq_tests = rbind(chisq_tests, data.frame(player_id = player, chisq = tst$statistic, p_val = tst$p.value))
+}
+chisq_tests # several of these are highly significant: people are not choosing an even balance of R, P, S
 
 
 ### Scores ###
@@ -169,8 +234,14 @@ plot.scores(data)
 # for 300 rounds, max: 900; min: -300; expected: 200
 
 
+### Effort ###
+# Are people trying throughout the experiment?
+# Note this should also be reflected in the response times
+plot.move.sequence(data) # entropy wobbles around by decile but doesnt appear to fall off a cliff for anybody
+
+
+
 # TODO
-# Longest sequence? 
 # Distribution of outcomes for each player? 
 
 
